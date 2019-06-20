7b859be5a8a311eef0c59231fe7563034e649e6f
duanyuting <duanyuting@utry.cn>
2019-06-20 11:31:22 +0800

    feat: 新增分页组件

---
+++ lib/components/TablePag/TablePag.vue
@@ -0,0 +1,44 @@
+<template>
+  <element-pagination
+    @size-change="paginationData.handleSizeChange"
+    @current-change="paginationData.handleCurrentChange"
+    :current-page="currentPage"
+    :page-sizes="pageSizes"
+    :page-size="pageSize"
+    :layout="layout"
+    :total="paginationData.total"
+    :background="background"
+  >
+  </element-pagination>
+</template>
+
+<script>
+import { Pagination } from 'element-ui'
+
+export default {
+  name: 'IceTablePage',
+  inheritAttrs: false,
+  components: { ElementPagination: Pagination },
+  props: {
+    paginationData: {
+      type: Object,
+      default: () => {},
+    },
+
+    /*是否添加背景颜色*/
+    background: Boolean,
+  },
+
+  data() {
+    const { paginationData } = this.$props
+    return {
+      pageSizes: paginationData.pageSizes || [20, 50, 100, 200],
+      pageSize: paginationData.pageSize || 20,
+      currentPage: paginationData.currentPage || 1,
+      layout: paginationData.layout || 'total, sizes, prev, pager, next, jumper',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
